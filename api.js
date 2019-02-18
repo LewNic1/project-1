e840ad1ee60febbc1e25d9b73f71663add05a23c
BryantBuilt <bryantp24@gmail.com>
2019-02-18 00:37:01 -0800

    seed.js and api.js into master branch

---
+++ api.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "yelpAPIKey": "ggjGZ7Ke4SFvR-CiWy5D62_b6C3AALsCd0mxDl1fVy5xjxVXkY_lPxIubyL3aM8B4RdXkqsrtu7dUHLchp-4wYfcNw-R8OjTx9-qncc9rx2Pk2Q03sPfyB8_v1JnXHYx",
+    "yelpURL": "https://api.yelp.com/v3/businesses/",
+}
