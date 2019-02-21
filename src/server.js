b5e68496ed9238bc21bea0d99db9280a5f1453f3
JJ <johnhenry@JJs-MacBook-Pro.local>
2019-02-21 15:07:13 -0800

    added landing page | reworked routing

--- src/server.js
+++ src/server.js
@@ -44,16 +44,27 @@ app.use(recommendRoute);
 
 //I am making use of the express function called express.static to view all static pages in the public folder.
 app.use(express.static('public'))
+
+app.get('/dashboard', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../views/index.html'))
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../views/landing.html'))
+})
+
 //Handler for 404 - Resource Not Found
 //when there is no middleware to hande what the user types in then this will run.
-app.use((req, res, next)=>{
-    res.status(404).send('Darnell thinks you might be lost... Therefore you are lost. Sorry there are no maps -__-.')
-})
+// app.use((req, res, next)=>{
+//     res.status(404).send('Darnell thinks you might be lost... Therefore you are lost. Sorry there are no maps -__-.')
+// })
 //Handler for Error 500
 app.use((err, req, res, next) =>{
     console.error(err.stack)
     res.sendFile(path.join(__dirname, '../public/500.html'))
 })
+
+
 
 
 
