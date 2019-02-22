5e1d6a601a04928a01ff1df9d0f6e4fe8167705c
Darnell <darnellatsimon@gmail.com>
2019-02-22 11:47:03 -0800

    updated Css and Get request

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -1,3 +1,5 @@
+console.log('windowload')
+
 // POPUP/LOGIN SECTION
 $( 'button' ).click( function() {
   $( '.pop-up' ).addClass( 'open' );
@@ -5,9 +7,13 @@ $( 'button' ).click( function() {
 $( '.pop-up .close' ).click( function() {
   $( '.pop-up' ).removeClass( 'open' );
 } );
+
+
 // VIDEO SECTION
 let video = document.querySelector( 'video' );
+video.play()
 const setVideoDimensions = () => {
+  console.log('videotrigger')
   if ( window.innerWidth / window.innerHeight > 16 / 9 ) {
      video.style.width = '100vw';
      video.style.height = 'calc(100vw * 9 / 16)';
@@ -17,7 +23,9 @@ const setVideoDimensions = () => {
   }
 };
 window.onresize = setVideoDimensions;
+
 setVideoDimensions();
+
 
 //ajax call to check password
 // on success res.redirect('/dashboard')
