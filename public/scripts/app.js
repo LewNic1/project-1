e01acd7d7844968f94601e32ffb73f65da159753
BryantBuilt <bryantp24@gmail.com>
2019-02-21 19:33:42 -0800

    POST WORKS!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,13 @@
 //Here is where the map ajax will go
 let recommendation_list = [];
+
 // - Setting up APIs
 $(document).ready(function(){
   console.log('Doc Ready');
-  
+  let $recDiv = $('#recList');
+  let recMarker;
   // Front end AJAX
+
   $('.theForm').on('submit', function(e) {
     e.preventDefault();
     let formData = $(this).serialize();
@@ -14,16 +17,14 @@ $(document).ready(function(){
       url: '/dashboard/recommend',
       data: formData,
       success: function(res) {
-        console.log("success!!", res)
+        console.log("success!!", res);
+        createRecItem(res);
       },
       error: function(err) {
         console.log("uh oh, something went wrong", err);
       }
     });
-  })
-
-  let $recDiv = $('#recList');
-
+  });
 
   $.ajax({
     method: "GET",
@@ -41,129 +42,124 @@ $(document).ready(function(){
 
 // Google Maps Functions
 
-  // function initAutocomplete() {
-    let map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.7909, lng: -122.4013},
-      zoom: 16,
-      mapTypeId: 'roadmap'
+  let map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 37.7909, lng: -122.4013},
+    zoom: 16,
+    mapTypeId: 'roadmap'
+  });
+
+  // Create the search box and link it to the UI element.
+  let input = document.getElementById('pac-input');
+  let searchBox = new google.maps.places.SearchBox(input);
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Creating pin info window
+  let infowindow = new google.maps.InfoWindow();
+  // Connecting place services
+  let service = new google.maps.places.PlacesService(map);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    let places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
     });
+    markers = [];
 
-    // Create the search box and link it to the UI element.
-    let input = document.getElementById('pac-input');
-    let searchBox = new google.maps.places.SearchBox(input);
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-    // Creating pin info window
-    let infowindow = new google.maps.InfoWindow();
-    // Connecting place services
-    let service = new google.maps.places.PlacesService(map);
-
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
-    });
-
-    let markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener('places_changed', function() {
-      let places = searchBox.getPlaces();
-
-      if (places.length == 0) {
+    // For each place, get the icon, name and location.
+    let bounds = new google.maps.LatLngBounds();
+    console.log(bounds);
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
         return;
       }
+      
+      // Set Place Details For POST to DB
+      $('#place-name').val(place.name);
+      $('#place-id').val(place.place_id);
+      $('#place-latitude').val(place.geometry.location.lat());
+      $('#place-longitude').val(place.geometry.location.lng());
 
-      // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
-      markers = [];
+      let icon = {
+        url: place.icon, // update with our own
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-      // For each place, get the icon, name and location.
-      let bounds = new google.maps.LatLngBounds();
-      console.log(bounds);
-      places.forEach(function(place) {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        
-        let recPlaceName = place.name;
-        // console.log(recPlaceName);
-        let recPlaceId = place.place_id;
-        // console.log(recPlaceId);
-        let recLatitude = place.geometry.location.lat();
-        // console.log(recLatitude);
-        let recLongitude = place.geometry.location.lng();
-        // console.log(recLongitude);
-
-        let icon = {
-          url: place.icon, // update with our own
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25)
-        };
-
-        // Create a marker for each place.
-        markers.push(new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location
-        }));
-        
-        console.log(markers);
-
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
-  // }
-  // initAutocomplete();
-
-  function loadRecommendations(json) {
-    let recMarker;
-    json.forEach((rec) => {
-      $recDiv.append(
-        `<div class='rec-item'>
-          <div class='rec-details'>
-            <h4>${rec.name}</h4>
-            <h5>${rec.description}</h5>
-            <p>${rec.author}</p>
-          </div>
-          <div class='rec-options'>
-            <a href="#" class='rec-edit'><i class="fas fa-pencil-alt"></i></a>
-            <a href="#" class='rec-delete'><i class="fas fa-trash-alt"></i></a>
-          </div>
-        </div>`
-        );
-      recMarker = new google.maps.Marker({
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
         map: map,
-        title: rec.name,
-        position: {
-          lat: parseFloat(rec.latitude),
-          lng: parseFloat(rec.longitude)
-        }
-      });
-      google.maps.event.addListener(recMarker, 'click', function() {
-        infowindow.setContent(
-          `<div>
-          <strong>${rec.name}</strong><br> 
-          ${rec.description}<br>
-          - ${rec.author}
-          </div>`);
-        infowindow.open(map, this);
-      });
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      
+      console.log(markers);
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  }); // End of searchBox functioanlity
+  
+  function createRecItem(rec) {
+    $recDiv.append(
+      `<div class='rec-item' id='${rec._id}'>
+        <div class='rec-details'>
+          <h4>${rec.name}</h4>
+          <h5>${rec.description}</h5>
+          <p>${rec.author}</p>
+        </div>
+        <div class='rec-options'>
+          <a href="#" class='rec-edit'><i class="fas fa-pencil-alt"></i></a>
+          <a href="#" class='rec-delete'><i class="fas fa-trash-alt"></i></a>
+        </div>
+      </div>`
+      );
+    recMarker = new google.maps.Marker({
+      map: map,
+      title: rec.name,
+      position: {
+        lat: parseFloat(rec.latitude),
+        lng: parseFloat(rec.longitude)
+      }
+    });
+    google.maps.event.addListener(recMarker, 'click', function() {
+      infowindow.setContent(
+        `<div>
+        <strong>${rec.name}</strong><br> 
+        ${rec.description}<br>
+        - ${rec.author}
+        </div>`);
+      infowindow.open(map, this);
     });
   };
 
-});
+  function loadRecommendations(json) {
+    json.forEach((rec)=> {createRecItem(rec)});
+    };
+
+}); // End of Doc Ready
 
 //------------------drop down functionality starts here------------------------------- 
 
